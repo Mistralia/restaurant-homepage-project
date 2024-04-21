61de4887b5651166dc93e703133ae9ffd959d07b
mistralia <4mistralia@gmail.com>
2024-04-21 22:35:01 +0300

    data removal in config file

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'mistralia', 'test123', 'restaurant');
+$conn = mysqli_connect('localhost', '******', '******', 'restaurant');
 
 if (!$conn) {
     echo 'Connection error: ' . mysqli_connect_error();
--- config/db_connect_php.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost', 'fnulswmy_tati', 'nyanyancat389', 'fnulswmy_Restaurant');
-
-if (!$conn) {
-    echo 'Connection error: ' . mysqli_connect_error();
-}
